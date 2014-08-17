8cc10f65ed6b04267e486dc17bd6eaa6efeeddcb
Ben Rosser <rosser.bjr@gmail.com>
2014-08-16 20:51:31 -0400

    Added new setup.py file, added configuration for building sphinx docs, close #5.

--- setup.py
+++ setup.py
@@ -1,24 +1,95 @@
-#!/usr/bin/env python
+# Based from http://pythonhosted.org/setuptools/setuptools.html#automatic-script-creation
 
-from distutils.core import setup
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
 
-long_description = """This is a Python library for accessing and retrieving links to comics from the
-xkcd webcomic by Randall Munroe. It is NOT endorsed or made by him, it's an
-entirely independent project.
+here = path.abspath(path.dirname(__file__))
 
-It makes use of the JSON interface to Randall's site to retrieve comic data.
-One can create comic objects manually using Comic(number), or can use the helper
-functions provided- getLatestComic(), getRandomComic(), and getComic()- to do
-this. Once you have a Comic object, you can access data from it using various
-provided methods."""
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+	long_description = f.read()
 
-setup(name='xkcd',
-	version='2.2',
-	description='Library to access xkcd.com',
+setup(
+	name='xkcd',
+
+	# Versions should comply with PEP440.  For a discussion on single-sourcing
+	# the version across setup.py and the project code, see
+	# http://packaging.python.org/en/latest/tutorial.html#version
+	version='2.3',
+
+	description="Library to access xkcd.com",
 	long_description=long_description,
-	author='Ben Rosser',
-	license="MIT",
-	author_email='rosser.bjr@gmail.com',
-	url="http://venus.arosser.com/projects/xkcd",
-	py_modules=['xkcd'])
 
+	# The project's main homepage.
+	url='https://github.com/TC01/xkcd',
+
+	# Author details
+	author = "Ben Rosser",
+	author_email='rosser.bjr@gmail.com',
+
+	# Choose your license
+	license='MIT',
+
+	# See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+	classifiers=[
+		# How mature is this project? Common values are
+		#   3 - Alpha
+		#   4 - Beta
+		#   5 - Production/Stable
+		'Development Status :: 5 - Production/Stable',
+
+		# Indicate who your project is intended for
+		'Intended Audience :: Developers',
+		'Intended Audience :: End Users/Desktop',
+		'Topic :: Software Development :: Libraries',
+		'Topic :: Internet :: WWW/HTTP',
+		'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+
+		# Pick your license as you wish (should match "license" above)
+		'License :: OSI Approved :: MIT License',
+
+		# Specify the Python versions you support here. In particular, ensure
+		# that you indicate whether you support Python 2, Python 3 or both.
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+	],
+
+	# What does your project relate to?
+	keywords='xkcd webcomic',
+
+	# You can just specify the packages manually here if your project is
+	# simple. Or you can use find_packages().
+#	packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+	packages=['xkcd'],
+
+	# List run-time dependencies here.  These will be installed by pip when your
+	# project is installed. For an analysis of "install_requires" vs pip's
+	# requirements files see:
+	# https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
+	#install_requires=['peppercorn'],
+
+	# If there are data files included in your packages that need to be
+	# installed, specify them here.  If using Python 2.6 or less, then these
+	# have to be included in MANIFEST.in as well.
+	#package_data={
+	#	'sample': ['package_data.dat'],
+	#},
+
+	# Although 'package_data' is the preferred approach, in some case you may
+	# need to place data files outside of your packages.
+	# see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+	# In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+	#data_files=[('my_data', ['data/data_file'])],
+
+	# To provide executable scripts, use entry points in preference to the
+	# "scripts" keyword. Entry points provide cross-platform support and allow
+	# pip to create the appropriate form of executable for the target platform.
+#	entry_points={
+#		'console_scripts': [
+#			'calcpkg=calcrepo.calcpkg:main',
+#		],
+#	},
+	)
