864a41d7f2a84c29125c9d46b20ab6b7cb90af22
Jon Champagne <jon@jchampagne.ca>
2020-04-16 10:47:11 -0400

    Add support for downloading 2x comics

--- tests/xkcd_test.py
+++ tests/xkcd_test.py
@@ -30,6 +30,18 @@ class TestXkcd(unittest.TestCase):
 		# Delete the downloaded file
 		os.remove(path)
 
+	def test_download_comic_2x(self):
+		# Try to download a 2x comic
+		dlname = "xkcd-unittestpython_environment_2x.png"
+		test = xkcd.getComic(1987)
+		test.download(outputFile=dlname, x2=True)
+
+		path = os.path.join(os.path.expanduser("~"), "Downloads", dlname)
+		self.assertTrue(os.path.exists(path))
+
+		# Delete the downloaded file
+		os.remove(path)
+
 	def test_whatif(self):
 		# Get a What If to test.
 		test = xkcd.getWhatIf(3)
